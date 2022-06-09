53a7f6307c761b703dc54b54b5610fcdf282136d
Beth Wilson <bethwilson777@gmail.com>
2022-06-09 09:31:35 -0600

    Adding another example of assigning a variable to an Object

--- object_literal.js
+++ object_literal.js
@@ -1,3 +1,4 @@
+// List of Grocery stores items, assigning a variable to an object. 
 var safeway = {
   apples: 50,
   bananas: 25,
@@ -11,3 +12,18 @@ console.log(Object.keys(safeway));
 console.log(Object.values(safeway));
 console.log(safeway.oranges = 23);
 console.log(safeway.apples = true);
+
+// Tweet example to assign a variable to an Object.
+var tweet1 = {
+  name: "Leira Sanchez",
+  email: "leia@test.com",
+  message: "Hello World",
+  active: true
+}
+
+console.log(Object.values(tweet1));
+console.log(Object.keys(tweet1));
+console.log(tweet1.name);
+console.log(tweet1.email);
+console.log(tweet1.message);
+console.log(tweet1.active);
