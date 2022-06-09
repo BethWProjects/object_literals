ba4f258187fd1fa3f8024e64198424df14c3ed2e
Beth Wilson <bethwilson777@gmail.com>
2022-06-09 09:17:17 -0600

    Initial commit

---
+++ object_literal.js
@@ -0,0 +1,13 @@
+var safeway = {
+  apples: 50,
+  bananas: 25,
+  watermelon: 10,
+  lemons: 100,
+  limes: 78
+}
+
+console.log(safeway.apples);
+console.log(Object.keys(safeway));
+console.log(Object.values(safeway));
+console.log(safeway.oranges = 23);
+console.log(safeway.apples = true);
